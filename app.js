b80b41966a3b09ebb952937b0418c8f03e5fc142
Edgar <marquez.edgar3@gmail.com>
2014-03-10 18:32:31 -0400

    Update3

---
+++ app.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+ //entering items to list and clearing text field
+$("button").on('click', function(){
+var txtval = $('#input').val();
+if(txtval == 0){
+	alert('Please Enter items first');
+}else{
+var list = $(".list");
+list.prepend('<p class = "items" style="display:block">' + txtval + '<button class ="delete"></button></p>');//FadeIn doesn't work.
+ $('#input').val('');
+}
+ });
+
+//entering items to list using "Enter"
+$("#input").keypress(function(event){
+   if(event.keyCode == 13){
+   	var txtval = $('#input').val();
+   	var list = $(".list");
+list.prepend('<p class = "items" style="display:block">' + txtval + '<button class ="delete"></button></p>');//FadeIn doesn't work.
+ $('#input').val('');
+ 
+	}
+});
+
+
+//deleting items
+$(document).on("click", ".delete", function(){
+   $(this).closest(".items").fadeOut(1000);
+});
+
+});
+
+
+
+
+
+ 
+ 
+
